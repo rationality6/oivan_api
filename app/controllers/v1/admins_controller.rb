04a9cd0dc4182a18ba978a4b2685be377b953df4
Hyun Ahn <codepawn@icloud.com>
2022-12-15 04:13:38 +0900

    added serializer user_list

--- app/controllers/v1/admins_controller.rb
+++ app/controllers/v1/admins_controller.rb
@@ -1,10 +1,12 @@
 class V1::AdminsController < ApplicationController
-  before_action :validation_create_user_params_present
+  before_action -> { has_permission?(user: current_user) }
+  before_action :validation_create_user_params_present, only: [:create]
 
   def index
     query_result = User.all
-    render json: query_result
-    # render json: query_result, each_serializer: UserListSerializer, root: 'user_list'
+    render json: query_result,
+           each_serializer: V1::UserListSerializer,
+           root: 'user_list'
   end
 
   # Teachers can assign Role (Teachers or Students) to Users.
