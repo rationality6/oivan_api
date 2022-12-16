8d7c8e006efef7c78d1873fd48c51da0a6d6916b
Hyun Ahn <codepawn@icloud.com>
2022-12-16 17:27:43 +0900

    added test show spec

--- spec/requests/tests_spec.rb
+++ spec/requests/tests_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Tests', type: :request do
   let!(:test_user) { create(:user, :skip_validate, :teacher) }
 
-  let!(:tests) { FactoryBot.create_list(:test, 5, user_id: test_user.id) }
-
   before {
     allow_any_instance_of(V1::TestsController).to receive(:authenticate_request!).and_return(true)
     allow_any_instance_of(V1::TestsController).to receive(:user_id).and_return(test_user.id)
   }
 
   it 'get list of test' do
+    tests = FactoryBot.create_list(:test, 5, user_id: test_user.id)
+
     get tests_path
     json_parsed_response = JSON.parse(response.body)
 
@@ -41,4 +41,21 @@ RSpec.describe 'Tests', type: :request do
     result = Test.where(id: test.id).present?
     expect(result).to eq(false)
   end
+
+  it 'test with questions and options' do
+    test = create(:test, :ruby_middle_test, user_id: test_user.id)
+    questions = FactoryBot.create_list(:question, 5, user_id: test_user.id, test_id: test.id)
+    FactoryBot.create_list(:option, 5, user_id: test_user.id, question_id: questions.first.id)
+
+    get test_path(test.id)
+
+    json_parsed_response = JSON.parse(response.body)
+
+    serial_option = { each_serializer: V1::TestWithQuestionSerializer, adapter: :attributes }
+    serials = ActiveModelSerializers::SerializableResource.new(test, serial_option)
+    result = JSON.parse(serials.to_json)
+
+    expect(json_parsed_response).to eq(result)
+  end
+
 end
