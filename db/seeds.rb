2b4d3e60fd8e411fd15b869d2672b6387e88aa77
Hyun Ahn <codepawn@icloud.com>
2022-12-23 00:18:51 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,81 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user0 = User.create({ email: 'teacher@example.com',
+                      password: 12345678,
+                      role: 0 })
 
-User.create({ email: 'teacher@example.com',
-              password: 12345678,
-              role: 0 })
+test0 = Test.create(
+  name: "Math 1",
+  description: "Take a Math test",
+  user_id: user0.id)
 
+question0 = Question.create(name: "Simple Algebra",
+                         description: "What is 2 + 2?",
+                         test_id: test0.id,
+                         user_id: user0.id)
+
+Option.create(
+  description: "The result is 2",
+  answer: false,
+  question_id: question0.id,
+  user_id: user0.id)
+
+Option.create(
+  description: "The result is 4",
+  answer: true,
+  question_id: question0.id,
+  user_id: user0.id)
+
+Option.create(
+  description: "The result is 0",
+  answer: false,
+  question_id: question0.id,
+  user_id: user0.id)
+
+test1 = Test.create(name: "English 1",
+                    description: "Test your English skills 1",
+                    user_id: user0.id)
+
+question1 = Question.create(name: "Simple_english",
+                         description: "What is meaning of life?",
+                         test_id: test1.id,
+                         user_id: user0.id)
+
+Option.create(
+  description: "The answer is 42",
+  answer: true,
+  question_id: question1.id,
+  user_id: user0.id)
+
+Option.create(
+  description: "The answer is 88",
+  answer: false,
+  question_id: question1.id,
+  user_id: user0.id)
+
+Option.create(
+  description: "The answer is 103",
+  answer: false,
+  question_id: question1.id,
+  user_id: user0.id)
+
+
+test2 = Test.create(name: "English 2",
+                    description: "Test your English skills 2",
+                    user_id: user0.id)
+
+
+question2 = Question.create(name: "Simple_english",
+                         description: "is apple fruit ?",
+                         test_id: test2.id,
+                         user_id: user0.id)
+
+Option.create(
+  description: "fruit",
+  answer: true,
+  question_id: question2.id,
+  user_id: user0.id)
+
+Option.create(
+  description: "vegetable",
+  answer: false,
+  question_id: question2.id,
+  user_id: user0.id)
